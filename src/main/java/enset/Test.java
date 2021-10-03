fb5920326875ff03cf964be41525c797d6d9a7cc
Angelus678 <angelstone78@hotmail.fr>
2021-10-03 22:42:12 +0200

    second

--- src/main/java/enset/Test.java
+++ src/main/java/enset/Test.java
@@ -5,7 +5,7 @@ public class Test{
 
         public static void main(String [] args){
 
-    cd         System.out.println("L'archange Raphaêl nous guérit");
+            System.out.println("L'archange Raphaêl nous guérit");
 
 
         }
