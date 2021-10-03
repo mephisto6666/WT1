c1dd3d29d9eefd777a81115fccc12c86b66c94a9
Angelus678 <angelstone78@hotmail.fr>
2021-10-03 19:50:22 +0200

    Initial version

---
+++ src/main/java/enset/Test.java
@@ -0,0 +1,19 @@
+package ma.enset;
+
+public class Test{
+
+
+        public static void main(String [] args){
+
+    cd         System.out.println("L'archange Raphaêl nous guérit");
+
+
+        }
+
+
+
+
+
+
+
+}
---
+++ src/main/java/enset/ma/Test.java
@@ -0,0 +1,19 @@
+package ma.enset;
+
+public class Test{
+
+
+        public static void main(String [] args){
+
+            System.out.println("L'archange Raphaêl nous guérit");
+
+
+        }
+
+
+
+
+
+
+
+}
